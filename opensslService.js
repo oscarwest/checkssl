e9bd272e60c644eb3c2f45f0a4613a2377f5af86
Andreas Antonsson <andreas.antonsson@collectorbank.se>
2018-11-12 11:00:46 +0100

    Moved getCertInfo per url to diffrent functuin

--- opensslService.js
+++ opensslService.js
@@ -1,21 +1,25 @@
 const shell = require('shelljs');
 
+getCertInfoPerUrl = (url) => {
+    var noSchemeUrl = url.replace(/(^\w+:|^)\/\//, '');
+    var exp = shell.exec(`echo | openssl s_client -servername ${noSchemeUrl} -connect ${noSchemeUrl}:${sslPort} 2>/dev/null | openssl x509 -noout -dates 2>/dev/null`);
+
+    var expRawString = expPattern.exec(exp.stdout);
+    var expDateObj = new Date(expRawString[1]).toISOString().replace(/:/g, '-')
+
+    return {
+        domain: url,
+        expires: expDateObj
+    };
+}
+
 getCertInfo = (urls) => {
     var res = [];
     const sslPort = 443;
     const expPattern = /notAfter=(.*)/;
 
     for (const url of urls) {
-        var noSchemeUrl = url.replace(/(^\w+:|^)\/\//, '');
-        var exp = shell.exec(`echo | openssl s_client -servername ${noSchemeUrl} -connect ${noSchemeUrl}:${sslPort} 2>/dev/null | openssl x509 -noout -dates 2>/dev/null`);
-
-        var expRawString = expPattern.exec(exp.stdout);
-        var expDateObj = new Date(expRawString[1]).toISOString().replace(/:/g, '-')
-
-        res.push({
-            domain: url,
-            expires: expDateObj
-        });
+        res.push(getCertInfoPerUrl(url));
     }
 
     return res;
